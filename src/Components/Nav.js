aaeda42e14a2667d1d4ecc20e9d35fb21d1054f6
saengmotmi <oj8mm@naver.com>
2020-02-22 16:12:33 +0900

    components 폴더 추가, main.js 작성 시작

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react'
 
-class Main extends Component {
-
+class Nav extends Component {
 
     render() {
         return(
-
+            
         );
     }
 }
 
-export default Main;
+export default Nav;

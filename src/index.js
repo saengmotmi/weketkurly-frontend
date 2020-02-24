1bd6e2b44221dcd11df135f61c8650afd56ad125
saengmotmi <oj8mm@naver.com>
2020-02-24 15:59:52 +0900

    초기 세팅 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Pages/Styles/index.css';
+import './Styles/index.scss';
 import Routes from './Routes';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));

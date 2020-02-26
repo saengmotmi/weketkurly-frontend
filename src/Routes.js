495ad64c33f3912710ae670e0b9d56af9c9ef719
eunjileee <leeeeunz@gmail.com>
2020-02-26 21:06:29 +0900

    routes 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from 'react-router-dom';
-import ProductDetail from './Pages/ProductDetail/ProductDetail.js';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Main from ".Pages/Main/Main.js";
+import ProductDetail from "./Pages/ProductDetail/ProductDetail.js";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={ProductDetail} />
+          <Route exact path="/" component={Main} />
+          <Route exact path="/product-detail" component={ProductDetail} />
         </Switch>
       </Router>
-    )
+    );
   }
 }
 

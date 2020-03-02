6f3f5f8323cc5ef4523d39766093e4f309e51761
eunjileee <leeeeunz@gmail.com>
2020-03-03 01:18:17 +0900

    css 수정

--- src/Pages/OrderList/OrderList.js
+++ src/Pages/OrderList/OrderList.js
@@ -79,51 +79,60 @@ export default class OrderList extends Component {
               <div>1:1 문의하기</div>
             </div>
           </aside>
-
           <article>
-            <div className="title-bar">
-              <div>
+            <div className="order-list">
+              <div className="title-bar">
                 <h2>주문내역</h2>
                 <span className="sub">
                   지난 3년간의 주문 내역 조회가 가능합니다
                 </span>
               </div>
-              <select>
-                <option>전체기간</option>
-                <option>2020년</option>
-                <option>2019년</option>
-                <option>2018년</option>
-              </select>
+              <div>
+                <select>
+                  <option>전체기간</option>
+                  <option>2020년</option>
+                  <option>2019년</option>
+                  <option>2018년</option>
+                </select>
+              </div>
             </div>
+            {/* 상품 컴포넌트 */}
             <div className="product">
-              <h3 className="data">2020.02.08(19시 34분)</h3>
-              <ul>
-                <li className="list">
-                  <div className="product-name">
-                    [네떼] 파프리카샐러드 65g 외 5건
-                  </div>
-                  <div>
-                    <div className="thumb" />
-                    <div>
-                      <dl>
-                        <dt>주문번호</dt>
-                        <dd>1581158003681</dd>
-                      </dl>
-                      <dl>
-                        <dt>걸제금액</dt>
-                        <dd>31,880원</dd>
-                      </dl>
-                      <dl>
-                        <dt>주문산택</dt>
-                        <dd>배송완료</dd>
-                      </dl>
+              <div className="date">2020.02.08(19시 34분)</div>
+              <div className="table">
+                <ul>
+                  <li>
+                    <div className="name">[네떼] 파프리카샐러드 65g 외 5건</div>
+                    <div className="product-info">
+                      <div className="info">
+                        <div className="thumb" />
+                        <div>
+                          <dl>
+                            <dt>주문번호</dt>
+                            <dd>1581158003681</dd>
+                          </dl>
+                          <dl>
+                            <dt>걸제금액</dt>
+                            <dd>31,880원</dd>
+                          </dl>
+                          <dl>
+                            <dt>주문선택</dt>
+                            <dd>배송완료</dd>
+                          </dl>
+                        </div>
+                      </div>
+                      <div className="button">
+                        <button className="write">후기쓰기</button>
+                        <button className="question">1:1 문의</button>
+                      </div>
                     </div>
-                  </div>
-                </li>
-              </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
           </article>
         </div>
+        <Footer />
       </div>
     );
   }

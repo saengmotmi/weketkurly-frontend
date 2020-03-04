f49e0c2094c3ee09ea9fa9549126e4b8e5b789fa
eunjileee <leeeeunz@gmail.com>
2020-03-04 20:01:18 +0900

    마이컬리 css 수정

--- src/Pages/OrderList/OrderList.js
+++ src/Pages/OrderList/OrderList.js
@@ -61,29 +61,23 @@ export default class OrderList extends Component {
           <aside className="my-kurly">
             <h2>마이컬리</h2>
             <ul>
-              <li>
+              <li className="list">
                 <span>주문내역</span>
-                <div className="arrow" />
               </li>
-              <li>
+              <li className="_list">
                 <span>늘 사는 것</span>
-                <div className="arrow" />
               </li>
-              <li>
+              <li className="_list">
                 <span>상품후기</span>
-                <div className="arrow" />
               </li>
-              <li>
+              <li className="_list">
                 <span>적립금</span>
-                <div className="arrow" />
               </li>
-              <li>
+              <li className="_list">
                 <span>쿠폰</span>
-                <div className="arrow" />
               </li>
-              <li>
+              <li className="_list">
                 <span>개인정보수정</span>
-                <div className="arrow" />
               </li>
             </ul>
             <div className="help">

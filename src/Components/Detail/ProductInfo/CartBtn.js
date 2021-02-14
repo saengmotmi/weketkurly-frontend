19fb6695977a52b6ffb2e1b233ad3588ab135c65
오종택 <ohjtack@gmail.com>
2021-02-15 03:55:10 +0900

    wip

--- src/Components/Detail/ProductInfo/CartBtn.js
+++ src/Components/Detail/ProductInfo/CartBtn.js
@@ -9,7 +9,7 @@ export default class CartBtn extends Component {
         <div className="cart-btn">
           <button className="alarm-off">재입고 알림</button>
           <button
-            className={save ? "save-item-clicked" : "save-item"}
+            className={save ? "save-item clicked" : "save-item"}
             onClick={handleOnClickSave}
           >
             늘 사는것
